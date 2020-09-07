eb1c1ada4c013adfd5f4ea23fdb0fd810ffe15f5
ShoaibBadhe <69689997+ShoaibBadhe@users.noreply.github.com>
2020-09-07 10:42:27 +0530

    Add files via upload

---
+++ qrcode.py
@@ -0,0 +1,18 @@
+import cv2
+from pyzbar.pyzbar import decode
+
+cap = cv2.VideoCapture(0)
+font=cv2.FONT_HERSHEY_PLAIN
+
+while(True):
+	_,frame = cap.read()
+	
+	decodeobj = decode(frame)
+	for obj in decodeobj:
+		cv2.putText(frame,str(obj.data),(50,50),font,3,(255,0,0),3)
+	
+	cv2.imshow('Frame',frame)
+	
+	key = cv2.waitKey(1)
+	if key==27:
+		break
