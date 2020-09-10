6935fce5c834a0b23121c864f47f7448c804cbd4
ShoaibBadhe <69689997+ShoaibBadhe@users.noreply.github.com>
2020-09-10 10:29:11 +0530

    Add files via upload
    This code helps to scan the qr code and print the data once.

---
+++ qrcode2.py
@@ -0,0 +1,19 @@
+import cv2
+from pyzbar.pyzbar import decode
+
+def scan():
+    cap = cv2.VideoCapture(0)
+    i=0
+    while i<1:
+        _,frame = cap.read()
+	
+        decodeobj = decode(frame)
+        for obj in decodeobj:
+            print(obj.data)
+            i+=1
+        cv2.imshow('Frame',frame)
+	
+        cv2.waitKey(2)
+        cv2.destroyAllWindows
+
+scan()
